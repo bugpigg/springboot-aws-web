1d30ce32706f668bbbf03fc1082693cbf41e5667
bugpigg <bugpigg@gmail.com>
2022-02-27 22:27:56 +0900

    Jpa Auditing 테스트 코드 작성

--- src/test/java/com/bugpigg/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/bugpigg/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.bugpigg.book.springboot.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
@@ -23,7 +24,7 @@ class PostsRepositoryTest {
     }
 
     @Test
-    public void 게시글저장_불러오기() {
+    void 게시글저장_불러오기() {
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
@@ -43,4 +44,19 @@ class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }

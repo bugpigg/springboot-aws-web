92fa75abb7f4f6c3af68b78c280e055d3737c84e
bugpigg <bugpigg@gmail.com>
2022-02-28 12:11:15 +0900

    mustache 를 이용한 기본 페이지 생성 및 테스트 코드 작성
    - index.mustache 에 기본 페이지 생성
    - URL 매핑을 위해 IndexController 클래스 생성
    - 테스트 코드를 통한 기본 페이지 생성 검증

---
+++ src/main/java/com/bugpigg/book/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.bugpigg.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // 앞의 경로와, 뒤의 확장자는 자동으로 지정된다
+    }
+}

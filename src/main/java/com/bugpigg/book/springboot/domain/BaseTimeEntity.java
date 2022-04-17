814a3adb61691a42a490da7f51f0355bdba3b0c7
bugpigg <bugpigg@gmail.com>
2022-04-17 18:57:17 +0900

    docs : JPA Auditing 관련 주석 수정

--- src/main/java/com/bugpigg/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/bugpigg/book/springboot/domain/BaseTimeEntity.java
@@ -9,8 +9,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
-@MappedSuperclass // JPA Entity 들이 BaseTimeEntity 를 상속할 경우 필드들도 칼럼으로 인식하도록 한다.
-@EntityListeners(AuditingEntityListener.class) // 이 클래스에 Auditing 기능을 포함시킵니다. // 자동으로 시간을 넣어준다.
+@MappedSuperclass // JPA Entity 들이 BaseTimeEntity 를 상속할 경우 이 클래스의 필드들도 칼럼으로 인식하도록 한다.
+@EntityListeners(AuditingEntityListener.class) // 이 클래스에 Auditing 기능을 포함시킵니다. // Entity 영속성 및 업데이트에 대한 Auditing 정보를 캡처하는 JPA Entity Listener
 public abstract class BaseTimeEntity { // 모든 Entity 의 상위 클래스가 되어 Entity 들의 createdDate, modifiedDate 를 자동으로 관리해준다.
 
     @CreatedDate
